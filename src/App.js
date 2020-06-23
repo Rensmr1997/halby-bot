74a1bf09093557f927aaab56477f281b6069abaf
Rensmr1997 <39047525+Rensmr1997@users.noreply.github.com>
2020-06-23 12:46:17 +0200

    Styling/indeling homepage

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-       <KommunicateChat/>   
-        
+       <KommunicateChat/>  
         <nav class="navbar">
           <a class="navbar-brand">
-            <img src="../logo.png" width="40" height="40" />
+            <img src="../logo.svg" width="40" height="40" />
           </a>
         </nav>
-                <section className="col-12 homepage-header justify-content-center">
+        <section className="col-12 homepage-header justify-content-center">
           <div className="spotlight-holder">
             <div className="spotlight-content">
               <div className="main-text"></div>
@@ -22,6 +21,52 @@ function App() {
                 <h2>Hoe kunnen we u helpen?</h2>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="homepage-content">
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Lorem Ipsum</h2>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+            <div className="col-md-4">
+              <h2>Lorem Ipsum</h2>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+            <div className="col-md-4">
+              <h2>Lorem Ipsum</h2>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="homepage-footer">
+          <div className="row">
+            <div className="col-md-6">Contactgegevens</div>
+            <div className="col-md-6">Andere info</div>
           </div>
         </section>
       </div>
