f9efdbda830b6076440515909a11e9400a4d23cd
Rensmr1997 <39047525+Rensmr1997@users.noreply.github.com>
2020-06-23 12:02:41 +0200

    Header aangemaakt

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ function App() {
       <div className="container-fluid">
        <KommunicateChat/>   
         
+        <nav class="navbar">
+          <a class="navbar-brand">
+            <img src="../logo.png" width="40" height="40" />
+          </a>
+        </nav>
+                <section className="col-12 homepage-header justify-content-center">
+          <div className="spotlight-holder">
+            <div className="spotlight-content">
+              <div className="main-text"></div>
+              <h1>HLB helpt u uw doelen te realiseren</h1>
+              <div className="sub-text">
+                <h2>Hoe kunnen we u helpen?</h2>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div> 
 
